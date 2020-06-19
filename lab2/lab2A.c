3419f8c69f2fe1e823dc914d69ced7b4ccbaf934
Rajwinder <49246284+prezi17@users.noreply.github.com>
2020-06-19 16:06:36 -0400

    Update lab2A.c

--- lab2/lab2A.c
+++ lab2/lab2A.c
@@ -1,11 +1,4 @@
 
-/***************************************
-* EECS2031B - Lab2 *
-* Author: Rajwinder kaur, Rajwinder kaur *
-* Email: rkaur3@my.yorku.ca	*
-* eecs_username: rkaur3 *
-* York Student #: 216907602
-****************************************/
 #include<stdio.h>
 
 
